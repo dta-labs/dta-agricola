b0be1d38beebd203039191e6562cb5005c6a455c
DTA Labs <51190059+dta-labs@users.noreply.github.com>
2023-03-04 11:56:47 -0600

    Actualización PV66_tester

--- Embebido/DTA_Control_Riego_Pv66_tester/DTA_Control_Riego_Pv66_tester.ino
+++ Embebido/DTA_Control_Riego_Pv66_tester/DTA_Control_Riego_Pv66_tester.ino
@@ -4,7 +4,7 @@
  *                                                              *
  *            Tester de sistemas de Movimiento Continuo         *
  *                                                              *
- *   ~ Salidas x 8                                              *
+ *   ~ Salidas x 5                                              *
  *   ~ Entradas digitales x 2                                   *
  *   ~ Entradas analógicas x 2                                  *
  *   ~ Comunicación GSM                                         *
@@ -33,6 +33,7 @@ void setup() {
   }
   Serial.println(F(">>> DTA-Agrícola: Serie Pv66 v0.2.x"));
   Serial.println(F("    «Programa de prueba»"));
+  wdt_enable(WDTO_8S);
 }
 
 void loop() {
@@ -53,6 +54,7 @@ void testActuadores() {
     delay(500);
   }
   Serial.println();
+  systemWatchDog();
 }
 
 void testSensores() {
@@ -68,6 +70,7 @@ void testSensores() {
   Serial.print(F("  ~ Sensor A1 (Voltaje 2): ")); Serial.println(analogRead(A1));
   delay(500);
   Serial.println();
+  systemWatchDog();
 }
 
 void testComunicaciones() {
@@ -76,65 +79,66 @@ void testComunicaciones() {
   gprs.begin(9600);
   gprs.listen();
   gprs.println(F("AT+IPR=9600"));      // Velocidad en baudios?
-  getResponse(15, true); 
+  getResponse(15, true, "AT+IPR=9600"); 
   gprs.println(F("AT"));               // Tarjeta SIM Lista? OK
-  getResponse(15, true); 
+  getResponse(15, true, "AT"); 
   gprs.println(F("AT+CGMI"));          // Fabricante del dispositivo?
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CGMI"); 
   gprs.println(F("ATI"));              // Información del producto?
-  getResponse(15, true); 
+  getResponse(15, true, "ATI"); 
   gprs.println(F("AT+CGSN"));          // Número de serie?
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CGSN"); 
   gprs.println(F("AT+IPR?"));          // Velocidad en baudios?
-  getResponse(15, true); 
-  gprs.println(F("AT+CBC"));           // Estado de la bateriía
-  getResponse(15, true); 
+  getResponse(15, true, "AT+IPR?"); 
+  gprs.println(F("AT+CBC"));           // Estado de la batería
+  getResponse(15, true, "AT+CBC"); 
   gprs.println(F("AT+CFUN?"));         // Funcionalidad 0 mínima 1 máxima
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CFUN?"); 
   gprs.println(F("AT+CGATT=1"));       // Tarjeta SIM insetada? +CPIN: READY OK
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CGATT=1"); 
   gprs.println(F("AT+CPIN?"));         // Tarjeta SIM insetada? +CPIN: READY OK
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CPIN?"); 
   gprs.println(F("AT+WIND=1"));        // Indicación de tarjeta SIM insetada? +CPIN: READY OK
-  getResponse(15, true); 
+  getResponse(15, true, "AT+WIND=1"); 
   gprs.println(F("AT+CREG?"));         // Tarjeta SIM registrada? +CREG: 0,1 OK 
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CREG?"); 
   gprs.println(F("AT+CGATT?"));        // Tiene GPRS? +CGATT: 1 OK
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CGATT?"); 
   gprs.println(F("AT+CSQ"));           // Calidad de la señal -  debe ser 9 o superior: +CSQ: 14,0 OK
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CSQ"); 
   gprs.println(F("AT+CCLK?"));         // Fecha y Hora?
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CCLK?"); 
   gprs.println(F("AT+COPS?"));         // Comañía telefónica?
-  getResponse(15, true); 
+  getResponse(15, true, "AT+COPS?"); 
   gprs.println(F("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\""));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\""); 
   gprs.println(F("AT+SAPBR=3,1,\"APN\",\"internet.itelcel.com\""));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+SAPBR=3,1,\"APN\",\"internet.itelcel.com\""); 
   gprs.println(F("AT+SAPBR=3,1,\"USER\",\"webgpr\""));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+SAPBR=3,1,\"USER\",\"webgpr\""); 
   gprs.println(F("AT+SAPBR=3,1,\"PWD\",\"webgprs2002\""));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+SAPBR=3,1,\"PWD\",\"webgprs2002\""); 
   gprs.println(F("AT+CFUN=1"));        // Funcionalidad 0 mínima 1 máxima
-  getResponse(15, true); 
+  getResponse(15, true, "AT+CFUN=1"); 
   gprs.println(F("AT+SAPBR=1,1"));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+SAPBR=1,1"); 
   gprs.println(F("AT+SAPBR=2,1"));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+SAPBR=2,1"); 
   gprs.println(F("AT+HTTPINIT"));
-  getResponse(15, true); 
+  getResponse(15, true, "AT+HTTPINIT"); 
   gprs.println(F("http://pprsar.com/cosme/commj_v2.php?id=111111111111&st=OFF&tm=0&po=0&rx=Er&si=19"));
   // gprs.println(F("http://dtaamerica.com/ws/commj_v2.php?id=111111111111&st=OFF&tm=0&po=0&rx=Er&si=19"));
-  getResponse(25, true); 
+  getResponse(25, true, "http://pprsar.com/cosme/commj_v2.php?id=111111111111&st=OFF&tm=0&po=0&rx=Er&si=19"); 
   gprs.println(F("AT+HTTPACTION=0"));
-  getResponse(4000, true); 
+  getResponse(4000, true, "AT+HTTPACTION=0"); 
   gprs.println(F("AT+HTTPREAD"));
-  getResponse(0, true);
+  getResponse(0, true, "AT+HTTPREAD");
   gprs.println(F("AT+HTTPTERM"));
-  getResponse(30, true); 
+  getResponse(30, true, "AT+HTTPTERM"); 
 }
 
-String getResponse(int wait, bool response){
+String getResponse(int wait, bool response, String smg){
+  systemWatchDog();
   String result = "";
   delay(wait);
   unsigned long iTimer = millis();
@@ -146,8 +150,10 @@ String getResponse(int wait, bool response){
     delay(1.5);
   }
   if (response) {
+    result += result != "" ? "" : smg;
     Serial.println(result);
   }
+  systemWatchDog();
   return result;
 }
 
@@ -187,6 +193,7 @@ bool parseGPSData() {
       Serial.write(c);                      // descomentar para ver el flujo de datos del GPS
       if (gps.encode(c)) newData = true;    // revisa si se completó una nueva cadena
     }
+    systemWatchDog();
   }
   return newData;
 }
@@ -225,8 +232,8 @@ void testWatchdog() {
     delay(4000);
     systemWatchDog();
   }
-  Serial.print(F("\n    -> El sistema se reiniciará en 8 segundos"));
-  for (byte i = 0; i < 8; i++) {
+  Serial.print(F("\n    -> El sistema se reiniciará en 12 segundos"));
+  for (byte i = 0; i < 15; i++) {
     Serial.print(F("."));
     delay(1000);
   }
@@ -235,7 +242,8 @@ void testWatchdog() {
 void systemWatchDog() {
   wdt_reset();
   pinMode(watchDogPin, OUTPUT);         // Sink current to drain charge from C2
-  digitalWrite(watchDogPin, LOW);
-  delay(100);                           // Give enough time for C2 to discharge (should discharge in 50 ms)     
+  digitalWrite(watchDogPin, HIGH);
+  delay(50);                            // Give enough time for C2 to discharge (should discharge in 50 ms)     
+  // digitalWrite(watchDogPin, HIGH);
   pinMode(watchDogPin, INPUT);          // Return to high impedance
 }
