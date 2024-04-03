90700ae3a04e8c32f7ccd51e645746312bba7673
DTA Labs <51190059+dta-labs@users.noreply.github.com>
2024-04-02 23:49:19 -0600

    Versión 3 de sensores

---
+++ Embebido/LoRa/LoRa_Gateway_v3/LoRa_Gateway_v3.ino
@@ -0,0 +1,111 @@
+/*************************************************
+ *                   GATEWAY                     *
+ *************************************************/
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <SoftwareSerial.h>
+#include "LowPower.h"
+#include "miscelaneas.h"
+#include "configuracion.h"
+#include "comunicaciones.h"
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);  
+  Serial.print("\nLoRa Gateway: "); Serial.println(gatewayAddress);
+  if (!LoRa.begin(433E6)) { // 433E6 or 915E6, the MHz speed of module
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  comunicaciones(false);
+}
+ 
+void loop() {
+  sendRequests();
+}
+
+void sendRequests() {
+  String newNodeAddress = "DTA_" + sensorsID.substring(idx * 2, idx * 2 + 2);
+  Serial.print(F("nodeAddress: ")); Serial.print(newNodeAddress);
+  measurements[idx] = requestNode(newNodeAddress);
+  Serial.println();
+  idx++;
+  if (idx >= numSensors) {
+    sendDataHTTP();
+    sleepFor(sleepingTime);
+    Serial.println(F("\nNuevo ciclo..."));
+    // resetSoftware();
+    idx = 0;
+  }
+}
+
+float requestNode(String nodeAddress) {
+  float value = 0;
+  if (nodeAddress == (String)gatewayAddress) {
+    value = analogRead(sensor);
+  } else {
+    sendSingleRequest(nodeAddress);
+    String data = receiveData();
+    value = getDataValue(data, nodeAddress);
+    int iter = 1;
+    while (value == -99 && iter <= 10) { 
+      sendSingleRequest(nodeAddress);
+      data = receiveData();
+      value = getDataValue(data, nodeAddress); 
+      iter++;
+      delay(5);
+    }
+  }
+  if (value != -99) {  Serial.print(F(" = ")); Serial.print(value); }
+  return value;
+}
+
+void sendSingleRequest(String nodeAddress) {
+  String data = String(gatewayAddress) + "," + String(nodeAddress) + "," + String(sleepingTime);
+  LoRa.beginPacket();
+  LoRa.print(data);  
+  LoRa.endPacket();
+}
+
+String receiveData() {
+  String inString = "";
+  for (int loop = 1; loop < 50; loop++) {
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) { 
+      while (LoRa.available()) {
+        int inChar = LoRa.read();
+        inString += (char)inChar;
+      }
+      LoRa.packetRssi(); 
+    }
+    delay(10);
+  }
+  return inString;   
+}
+
+float getDataValue(String data, String nodeAddress) {
+  byte idx = data.indexOf(',');
+  String from = data.substring(0, idx);
+  String to = data.substring(idx + 1, data.indexOf(',', idx + 1));
+  idx = data.indexOf(',', idx + 1);
+  float dataValue = data.substring(idx + 1, data.length()).toFloat();
+  return from == nodeAddress && to == gatewayAddress ? dataValue : -99;
+}
+
+void sendDataHTTP() {
+  Serial.print(F("["));
+  for (int j = 0; j < numSensors; j++) {
+    Serial.print(measurements[j]); 
+    if (j < numSensors - 1) Serial.print(F(", ")); 
+  } 
+  Serial.println(F("]")); 
+  comunicaciones(true);
+}
+
+void sleepFor(float minutes) {
+  Serial.print(F("Sleeping for ")); Serial.print(minutes); Serial.println(F(" minutes..."));
+  delay(10);
+  for (int i = 0;  i  <=  15 * minutes; i++)
+    LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+}
---
+++ Embebido/LoRa/LoRa_Gateway_v3/configuracion.h
@@ -0,0 +1,51 @@
+/****************************************************************************
+ *                                                                          * 
+ *   Configuración: {GSMr, GSMt, PLT, Pais, Lada, Número(3), Número(4)}     *
+ *   - GSM: Rx, Tx (D2, D3) <= Chip azul | (D3, D2) <= Chip rojo            *
+ *   - A0: Entrada analógica                                                *
+ *   - Comunicación LoRA                                                    *
+ *     . D9 : RST                                                           *
+ *     . D10: SS                                                            *
+ *     . D11: MOSI                                                          *
+ *     . D12: MISO                                                          *
+ *     . D13: SCK                                                           *
+ *   - Vector de configuración config[]                                     *
+ *     {x, x, xxx, xx, xxx, xxx, xxxx}                                      *
+ *      |  |   |   |   |    |     └ Número parte 2                          *
+ *      |  |   |   |   |    └────── Número parte 1                          *
+ *      |  |   |   |   └─────────── Lada                                    *
+ *      |  |   |   └─────────────── País                                    *
+ *      |  |   └─────────────────── Cantidad de sensores (1 ~ 254)          *
+ *      |  └─────────────────────── Tx                                      *
+ *      └────────────────────────── Rx                                      *
+ *                                                                          *
+ ****************************************************************************/
+
+// Settings
+const int config[] = {2, 3, 1, 33, 333, 333, 3333};
+
+#pragma region Variables
+
+#define telefono fillNumber(config[3], 2) + fillNumber(config[4], 3) + fillNumber(config[5], 3) + fillNumber(config[6], 4)
+#define httpServer F("AT+HTTPPARA=\"URL\",\"http://dtaamerica.com/ws/sensor_v1.php?id=")
+
+#define gatewayAddress "DTA_FF"
+// #define baseNodeAddress "DTA_00"
+// String nodeAddress = String(baseNodeAddress);
+
+#define sensor A0
+int numSensors = 1;
+float measurements[20];
+float voltages[20];
+String sensorsID = "";
+static int sleepingTime = config[2];
+static int idx = 0;
+
+SoftwareSerial gprs(config[0], config[1]);     // Comunicaciones
+bool restartGSM = true;
+byte signalVar = 0;
+byte commError = 0;
+bool commRx = true;
+bool testComm = false;
+
+#pragma endregion Variables
