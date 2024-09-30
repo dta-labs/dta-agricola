f39f506c8de0c8391f6a6fca14e64c3abc60a8f2
DTA Labs <51190059+dta-labs@users.noreply.github.com>
2024-09-30 07:36:33 -0600

    Pruebas de campo

---
+++ Embebido/Sensors/LoRa_Node_v3_Presure/configuracion.h
@@ -0,0 +1,7 @@
+/*************************************************
+ *               ADDRESS DEFINITION              *
+ *************************************************/
+
+#define gatewayAddress "DTA_FF"
+#define nodeAddress "DTA_01"
+#define sensor A0
--- Embebido/Sensors/ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ Embebido/Sensors/ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -1,11 +1,12 @@
+#define vMax 4.78
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
 }
 
 void loop() {
   float voltage = getVoltage();
-  Serial.println(voltage);
+  showData(voltage);
   delay(1000);
 }
 
@@ -13,8 +14,14 @@ float getVoltage() {
   float voltage = 0.0;
   for (int i = 0; i < 100; i++) {
     int sensorValue = analogRead(A0);
-    voltage += sensorValue * (5.0 / 1023.0);
+    voltage += sensorValue * (vMax / 1023.0);
     delay(5);
   }
   return voltage / 100.0;
+}
+
+void showData(float voltage) {
+  float bar = voltage * 16.0 / vMax;
+  float psi = bar * 14.503773773;
+  Serial.print("Voltage: "); Serial.print(voltage); Serial.print("V -> "); Serial.print(bar); Serial.print("bar -> "); Serial.print(psi); Serial.println("psi");
 }
