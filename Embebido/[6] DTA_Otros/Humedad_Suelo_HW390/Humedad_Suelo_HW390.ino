39d46a0e9430e6c433acb1a4a6161d277c1be86a
DTA Labs <51190059+dta-labs@users.noreply.github.com>
2025-04-14 19:36:13 -0500

    Humedad_Suelo_HW390

---
+++ Embebido/[6] DTA_Otros/Humedad_Suelo_HW390/Humedad_Suelo_HW390.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(A1, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(A1, HIGH);
+  delay(50);
+  int valorAnalogico = analogRead(A0);
+  digitalWrite(A1, LOW);
+  Serial.println(valorAnalogico);
+  delay(500);
+}
