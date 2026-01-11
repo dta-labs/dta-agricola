9fe34108fda2d1aebf92fd67f29579ca2924e39b
DTA Labs <51190059+dta-labs@users.noreply.github.com>
2026-01-11 02:05:45 -0600

    Humedad_Suelo_HW390_v2

--- Embebido/[5] DTA_Herramientas/Humedad_Suelo_HW390_v2/Humedad_Suelo_HW390_v2.ino
+++ Embebido/[5] DTA_Herramientas/Humedad_Suelo_HW390_v2/Humedad_Suelo_HW390_v2.ino
@@ -1,42 +1,66 @@
 #define sensorPin A0
 
-#define vSueloSeco 2815
-#define vSueloCC 1425
+struct Perfil {
+  float vSeco;  // voltaje en suelo seco 
+  float vCC;    // voltaje en capacidad de campo 
+  float vSat;   // voltaje en saturación 
+  int   pSeco;  // % en seco (ej. 25–30) 
+  int   pCC;    // % en CC (ej. 35–40) 
+  int   pSat;   // % en saturación (ej. 55–60)
+};
+
+Perfil arenoso = {2.95, 1.25, 0.70, 28, 35, 55};
+Perfil franco = {2.85, 1.15, 0.65, 30, 40, 58};
+Perfil arcilloso = {2.75, 1.05, 0.60, 32, 42, 60};
+
+Perfil mezcla(Perfil A, Perfil B, Perfil C, float wA, float wB) {
+  float wC = 1 - (wA + wB);
+  Perfil m;
+  m.vSeco = wA * A.vSeco + wB * B.vSeco + wC * C.vSeco;
+  m.vCC   = wA * A.vCC   + wB * B.vCC   + wC * C.vCC;
+  m.vSat  = wA * A.vSat  + wB * B.vSat  + wC * C.vSat;
+  m.pSeco = (int)(wA * A.pSeco + wB * B.pSeco + wC * C.pSeco);
+  m.pCC   = (int)(wA * A.pCC   + wB * B.pCC   + wC * C.pCC);
+  m.pSat  = (int)(wA * A.pSat  + wB * B.pSat  + wC * C.pSat);
+  return m;
+};
+
+Perfil suelo = { 2.82, 1.60, 1.07, 0, 40, 60 };
+// Perfil suelo = mezcla(arenoso, franco, arcilloso, 0.3, .1);
 
 #define DESV_EST_UMBRAL 0.3
 #define NUM_LECTURAS 30
 float hum_hist[NUM_LECTURAS] = {0};
 uint8_t index = 0;
-bool calentado = false;
 
 void setup() {
   pinMode(A1, OUTPUT);
-  analogReference(DEFAULT);
+  analogReference(DEFAULT);   // usa Vcc como referencia (3.3V o 5V)
   Serial.begin(19200);
 }
 
 void loop() {
-  getMoisture();
+  float lectura, vwc;
+  getMoisture(lectura, vwc);
+  Serial.println("Lectura: " + String(lectura, 3) + 
+                 "V ~> VWC: " + String(vwc) + "%");
 }
 
 #pragma region HW390
 
-void getMoisture() {
+void getMoisture(float &lectura, float &vwc) {
   static float muestras[NUM_LECTURAS];
   digitalWrite(A1, HIGH);
   delay(50);
   for (byte i = 0; i < NUM_LECTURAS; i++) {
-    muestras[i] = analogRead(sensorPin); // Leer el valor del sensor
+    muestras[i] = analogRead(sensorPin);
     delay(150);
   }
   digitalWrite(A1, LOW);
   int val = estimadorAdaptativo(muestras, NUM_LECTURAS);
-  float volt = getVcc();
-  float value = (val / 1023.0) * volt * 1000; 
-  float valor = value / 1000;
-  // float percent = constrain(map(value, valAire, valAgua, 38.0, 100.0), 0, 100);
-  float percent = constrain(map(value, vSueloSeco, vSueloCC, 0.0, 31.6), 0.0, 100.0);
-  Serial.println("Vcc: " + String(volt) + " ADC: " + String(val) + " Valor: " + String(valor, 3) + "V ~> " + String(percent, 1) + "%");
+  float volt = getVcc(); // mide referencia real
+  lectura = (val / 1023.0) * volt; // convierte a voltios
+  vwc = map(lectura * 1000, suelo.vSeco * 1000, suelo.vSat * 1000, suelo.pSeco, suelo.pSat);
 }
 
 float getVcc() {
@@ -46,11 +70,12 @@ float getVcc() {
   while (bit_is_set(ADCSRA, ADSC));
   long result = ADCL;
   result |= ADCH << 8;
-  result = 1126400L / result;             // Back-calculate AVcc in mV
+  result = 1126400L / result; // calcula AVcc en mV
   return result / 1000.0;
 }
 
 #pragma endregion HW390
+
 
 #pragma region Estadísticas
 
